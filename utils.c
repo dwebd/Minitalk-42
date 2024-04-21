9be6ac9f7b4a81b12ba91ec623c00468478809ee
dwebd <119140552+dwebd@users.noreply.github.com>
2024-04-21 12:39:36 +0400

    Update utils.c

--- utils.c
+++ utils.c
@@ -12,14 +12,6 @@
 
 #include "minitalk.h"
 
-int	ft_isalpha(int c)
-{
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return (1);
-	else
-		return (0);
-}
-
 int	ft_atoi(const char *str)
 {
 	long		res;
